2cee83eef3155f21d1e90dbe4a8c4b305618b45b
Maxim Manuylov <maxim.manuylov@gmail.com>
2014-04-10 01:54:08 +0200

    code

---
+++ server/src/jetbrains/buildServer/iaa/NewTestsAndProblemsProcessor.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.iaa;
+
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.serverSide.SRunningBuild;
+import jetbrains.buildServer.serverSide.STestRun;
+import jetbrains.buildServer.serverSide.impl.problems.BuildProblemImpl;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Maxim.Manuylov
+ *         Date: 10.04.2014
+ */
+public interface NewTestsAndProblemsProcessor {
+  void onTestFailed(@NotNull SRunningBuild build, @NotNull STestRun testRun);
+
+  void onBuildProblemOccurred(@NotNull SBuild build, @NotNull BuildProblemImpl problem);
+}
