0866b38c6108df31b98790aba782a9323fd4d408
grigory.panov <gripanov@gmail.com>
2019-05-22 20:00:34 +0300

    Rename "assign on second failure" param (previous name-delayAssignments)

--- src/main/java/jetbrains/buildServer/investigationsAutoAssigner/common/Constants.java
+++ src/main/java/jetbrains/buildServer/investigationsAutoAssigner/common/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
   public static final String USERS_TO_IGNORE = "excludeAssignees.usernames";
   public static final String SHOULD_IGNORE_COMPILATION_PROBLEMS = "ignoreBuildProblems.compilation";
   public static final String SHOULD_IGNORE_EXITCODE_PROBLEMS = "ignoreBuildProblems.exitCode";
-  public static final String SHOULD_DELAY_ASSIGNMENTS = "delayAssignments.enabled";
+  public static final String SHOULD_DELAY_ASSIGNMENTS = "assignOnSecondFailure";
 
   // Build configuration parameter and internal property
   public static final String DEFAULT_SILENT_MODE_ENABLED = "teamcity.investigationsAutoAssigner.suggestions.enabledByDefault";
